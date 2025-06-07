de718d1ab714a3599d280aff389f80293b924ad1
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2025-06-07 10:33:15 +0300

    Switch to uv

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 
-nox.options.default_venv_backend = "venv"
+nox.options.default_venv_backend = "uv"
 nox.options.reuse_existing_virtualenvs = True
 
 PYPROJECT = nox.project.load_toml("pyproject.toml")
@@ -8,16 +8,28 @@ PYTHON_VERSIONS = nox.project.python_versions(PYPROJECT)
 
 
 @nox.session(python=PYTHON_VERSIONS[0])
-def lint(session):
-    session.install(".[dev,test]")
+def lint(session: nox.Session) -> None:
+    session.run_install(
+        "uv",
+        "sync",
+        "--group=lint",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
     session.run("ruff", "format", "--check", "src", "tests")
     session.run("ruff", "check", "src", "tests")
     session.run("mypy", "src")
 
 
 @nox.session(python=PYTHON_VERSIONS)
-def test(session):
-    session.install(".[test]")
+def test(session: nox.Session) -> None:
+    session.run_install(
+        "uv",
+        "sync",
+        "--group=test",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
     session.run(
         "pytest",
         "--cov=memoized_lambda",
